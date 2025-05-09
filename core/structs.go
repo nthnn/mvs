57b41148d014ce098b00abb35e91276331ad1437
Nathanne Isip <nathanneisip@gmail.com>
2025-05-09 10:55:55 +0800

    Data structures for global configuration, file entry, and commits.

---
+++ core/structs.go
@@ -0,0 +1,27 @@
+package core
+
+import "os"
+
+type GlobalConfig struct {
+	Name          string `yaml:"name"`
+	Email         string `yaml:"email"`
+	PublicKey     string `yaml:"public_key"`
+	PrivateKey    string `yaml:"private_key"`
+	DefaultBranch string `yaml:"branch"`
+}
+
+type FileEntry struct {
+	Path string      `msgpack:"path"`
+	Mode os.FileMode `msgpack:"mode"`
+	Hash string      `msgpack:"hash"`
+}
+
+type Commit struct {
+	Hash      string      `msgpack:"hash"`
+	Parent    string      `msgpack:"parent"`
+	Message   string      `msgpack:"message"`
+	Author    string      `msgpack:"author"`
+	Email     string      `msgpack:"email"`
+	Timestamp string      `msgpack:"when"`
+	Files     []FileEntry `msgpack:"files"`
+}
