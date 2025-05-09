7c996b1e0472d7dc69f13f096e7ea2d5b088b4a1
Nathanne Isip <nathanneisip@gmail.com>
2025-05-09 10:58:42 +0800

    Load and save repository index function implementations.

---
+++ utils/index.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"os"
+	"strings"
+
+	"github.com/nthnn/mvs/core"
+)
+
+func LoadIndex() map[string]bool {
+	indices := map[string]bool{}
+	bytes, _ := os.ReadFile(core.IndexFile)
+
+	for _, l := range strings.Split(
+		string(bytes),
+		"\n",
+	) {
+		if l != "" {
+			indices[l] = true
+		}
+	}
+
+	return indices
+}
+
+func SaveIndex(idx map[string]bool) {
+	var list []string
+	for item := range idx {
+		list = append(list, item)
+	}
+
+	AtomicWriteFile(
+		core.IndexFile,
+		[]byte(strings.Join(list, "\n")),
+		0644,
+	)
+}
