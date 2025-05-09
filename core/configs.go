c37120939ed5c966c6a3c9c83a29329fb693fe36
Nathanne Isip <nathanneisip@gmail.com>
2025-05-09 10:56:19 +0800

    Configuration file definitions and load function implementation.

---
+++ core/configs.go
@@ -0,0 +1,50 @@
+package core
+
+import (
+	"os"
+
+	"github.com/nthnn/mvs/logger"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	RepoDir       = ".mvs"
+	ObjectsPath   = ".mvs/objects"
+	CommitsPath   = ".mvs/commits"
+	RefsPath      = ".mvs/refs/heads"
+	HeadFile      = ".mvs/HEAD"
+	IndexFile     = ".mvs/index"
+	KeyDirectory  = ".mvs/keys"
+	PrivateKeyPem = ".mvs/keys/id_ed25519"
+	PublicKeyPem  = ".mvs/keys/id_ed25519.pub"
+
+	GlobalConf = "~/.local/mvs/globals.yaml"
+)
+
+var GlobalConfiguration GlobalConfig
+
+func LoadGlobalConfig() {
+	data, err := os.ReadFile(GlobalConf)
+	if err != nil {
+		GlobalConfiguration.Email = "noemail@noemail.com"
+		GlobalConfiguration.PrivateKey = "MVS"
+		GlobalConfiguration.PublicKey = "MVS"
+
+		logger.Warning(
+			"Error: %v",
+			err,
+		)
+		return
+	}
+
+	var conf GlobalConfig
+	if err := yaml.Unmarshal(data, &conf); err != nil {
+		logger.Warning(
+			"Could not parse global config: %v",
+			err,
+		)
+		return
+	}
+
+	GlobalConfiguration = conf
+}
